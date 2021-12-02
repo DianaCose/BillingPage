653d487bc557d74a2287940eb4480d6e47b73ca5
DianaCose <92103409+DianaCose@users.noreply.github.com>
2021-12-02 15:08:19 +0200

    Initial setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Billing Page</title>
+      </Head>
+      <main>
+        Test 
+      </main>
+    </div>
+  );
+}
